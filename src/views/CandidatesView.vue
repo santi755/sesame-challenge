997061588dcbc8e7c206c8a326d4bfb555a2cf6b
Santiago Mansilla Labbé <santi755@gmail.com>
2024-06-26 23:02:00 +0200

    feat: Add lazy loading for images in CandidatesView

--- src/views/CandidatesView.vue
+++ src/views/CandidatesView.vue
@@ -11,4 +11,13 @@
 <script setup lang="ts">
 import TemplateBase from '@src/components/templates/TemplateBase.vue';
 import StatusPanel from '@src/components/patterns/candidate-status-panel/StatusPanel.vue';
+
+import { inject } from 'vue';
+import { TYPES } from '@core/config/types';
+import CreateCandidate from '@core/modules/vacancies/application/CreateCandidate';
+
+const inversifyContainer = inject('inversifyContainer');
+
+const createCandidate = inversifyContainer.get<CreateCandidate>(TYPES.CreateCandidate);
+createCandidate.execute();
 </script>
