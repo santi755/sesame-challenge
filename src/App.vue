407a84cd364efc8cbbe5571a196ee6fb278f3865
Santiago Mansilla Labbé <santi755@gmail.com>
2024-06-29 21:24:49 +0200

    Wait for vacancy to render the app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <router-view />
+  <!-- Implement loading status manager -->
+  <div v-if="vacancy">
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
 import { useVacancyStore } from '@src/stores/vacancy';
 
 const vacancyStore = useVacancyStore();
 vacancyStore.loadVacancy();
+
+const { vacancy } = storeToRefs(vacancyStore);
 </script>
