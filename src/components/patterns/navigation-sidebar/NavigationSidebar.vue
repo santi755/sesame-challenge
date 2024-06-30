0c0c3801d68a1b5c0413451f70d2420c971537af
Santiago Mansilla Labbé <santi755@gmail.com>
2024-06-30 11:32:21 +0200

    Logo design fix

--- src/components/patterns/navigation-sidebar/NavigationSidebar.vue
+++ src/components/patterns/navigation-sidebar/NavigationSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <SidebarBase class="bg-fontLight shadow-right py-2 px-2">
-    <div class="flex items-center justify-start w-full h-12 pb-2">
+    <div class="flex items-center justify-start w-full h-12 pb-2 px-3">
       <img src="@src/assets/sesamehr-logo.svg" alt="Logo" class="h-8" />
     </div>
 
