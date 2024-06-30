9f4df627342b12a1b61fe737ec182f7e9661da9b
Santiago Mansilla Labbé <santi755@gmail.com>
2024-06-30 17:43:33 +0200

    Added tabs options

---
+++ src/components/patterns/candidate-status-panel/OptionsStatusPanel.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- TODO: Make this component dynamic -->
+  <div class="flex px-6 justify-start border-b border-neutral200 leading-10">
+    <button class="mr-8">
+      <MediumBoldTypography class="option-selected text-fontLink">Vacantes</MediumBoldTypography>
+    </button>
+    <button>
+      <MediumTypography class="text-neutral200">Candidatos</MediumTypography>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MediumBoldTypography from '@src/components/atoms/typography/MediumBoldTypography.vue';
+import MediumTypography from '@src/components/atoms/typography/MediumTypography.vue';
+</script>
+
+<style>
+.option-selected {
+  position: relative;
+}
+
+.option-selected::after {
+  content: '';
+  position: absolute;
+  bottom: -0.063rem;
+  left: 0;
+  width: 100%;
+  height: 0.125rem;
+  border-radius: 0.125rem 0.125rem 0 0;
+  background-color: rgba(var(--fontLink));
+}
+</style>
--- src/components/patterns/candidate-status-panel/StatusPanel.vue
+++ src/components/patterns/candidate-status-panel/StatusPanel.vue
@@ -1,5 +1,8 @@
 <template>
   <TabPanel class="flex flex-col mt-2 flex-grow">
+    <template #options>
+      <OptionsStatusPanel class="mb-4" />
+    </template>
     <template #header>
       <HeaderStatusPanel class="mb-4" />
     </template>
@@ -11,6 +14,7 @@
 
 <script setup lang="ts">
 import TabPanel from '@src/components/atoms/panel/TabPanel.vue';
+import OptionsStatusPanel from '@src/components/patterns/candidate-status-panel/OptionsStatusPanel.vue';
 import HeaderStatusPanel from '@src/components/patterns/candidate-status-panel/HeaderStatusPanel.vue';
 import ContentStatusPanel from '@src/components/patterns/candidate-status-panel/ContentStatusPanel.vue';
 </script>
