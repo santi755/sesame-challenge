16f11d7d6a65c1111b4c5d066605953b3f5a529b
Santiago Mansilla Labbé <santi755@gmail.com>
2024-07-28 17:22:01 +0200

    feat: Add enum for environment options in EnvConfig.ts

--- core/config/env/EnvConfig.ts
+++ core/config/env/EnvConfig.ts
@@ -1,9 +1,15 @@
 // TODO: Refactor to type as enum
+enum EnvironmentOptions {
+  VITE_APP_API_URL = 'VITE_APP_API_URL',
+  VITE_APP_API_KEY = 'VITE_APP_API_KEY',
+  VITE_APP_VACANCY_ID = 'VITE_APP_VACANCY_ID'
+}
+
 export default class EnvConfig {
   static get(key: string): string {
     const env = import.meta.env;
 
-    if (!env[key]) {
+    if (!(key in EnvironmentOptions)) {
       throw new Error(`Env variable ${key} not found`);
     }
 
